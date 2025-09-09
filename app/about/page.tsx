1685c0a1ebcd542d56fd176a2044c6f0be8ea6c0
xvclenwn <144425999+xvclenwn@users.noreply.github.com>
2025-09-09 15:32:40 +0800

    Added Header and some css

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,14 @@
+import Header from "../components/Header";
+import styles from "./page.module.css";
+
 export default function About() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold text-blue-600">Миний тухай</h1>
-      <p className="mt-4 text-gray-700">
-        Би Next.js ашиглаж веб хөгжүүлэлт сурч байна.
-      </p>
-      <p className="mt-2">Миний хобби: унших, аялах, программчлал.</p>
-    </div>
-  )
+    <>
+      <Header />
+      <main className="p-6">
+        <h2 className={styles.title}>Миний тухай</h2>
+        <p className={styles.description}> Хүслэн гээд "Програмчин" залуу .</p>
+      </main>
+    </>
+  );
 }
